f8082a34deb263e2c1936a39f29ff2245e2c40a7
Diego Henrique <diegohrp20@gmail.com>
2021-07-24 23:43:49 -0300

    Primeiro commit. Criando uma simples API com swagger de login e registros de usuarios.

---
+++ cursoapi/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using cursoapi.Filters;
+using cursoapi.Models;
+using cursoapi.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cursoapi.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar!", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro interno.", Type = typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("logar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput) 
+        {
+            //if (!ModelState.IsValid) 
+            //{
+            //    return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
+            //}
+            return Ok(loginViewModelInput);
+        }
+
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar!", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro interno.", Type = typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("registrar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+    }
+}
